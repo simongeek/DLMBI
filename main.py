a26b51b0ef2df0a4a011ee14baa0e39d58b48a7c
simongeek <plotkaszymon@gmail.com>
2018-01-07 16:50:49 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Import necessary packages
+
+import allel
+import pandas as pd
+import numpy as np
+
+# Convert VCF to CSV file - DONE
+#allel.vcf_to_csv('chromosom22.vcf', 'chromosom22.csv')
+
+# Load our data as .csv file
+
+data = pd.read_csv('chromosom22.csv')
+
+######### Data transformation #########
+
+# print length of DataFrame (SNP = 1103548)
+
+total_rows = data.count()
+print(total_rows + 1)
+
+# drop unused columns: QUAL, FILTER_PASS, ID
+
+#columns = ['QUAL', 'FILTER_PASS', 'ID']
+
+#data.drop(columns, axis=1, inplace=True)
+
+# drop unused rows: ALT_2, ALT_3
+
+df1 = data[data['ALT_2'].str.len() > 0]
+df2 = data[data['ALT_3'].str.len() > 0]
+
+
+
+print(df1)
+
