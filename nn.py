07fe5455aeea4eb5867fae91c14f1f5d668544f6
Aleksandra <earrendil@gmail.com>
2018-01-22 20:09:47 +0100

    add deleting unnecessary variables

--- nn.py
+++ nn.py
@@ -1,16 +1,10 @@
-from sklearn import datasets, metrics
-from sklearn.model_selection import train_test_split, cross_validate
+from sklearn import  metrics
+from sklearn.model_selection import train_test_split
 import numpy as np
 import itertools
 import collections
 import pandas as pd
-
-# Define the non-linear functions used
-def logistic(z):
-    return 1 / (1 + np.exp(-z))
-
-def logistic_deriv(y):  # Derivative of logistic function
-    return np.multiply(y, (1 - y))
+from sklearn.metrics import f1_score, precision_score, recall_score
 
 def relu(z):
     return np.maximum(z, 0)
@@ -23,178 +17,124 @@ def relu_deriv(y):
 def softmax(z):
     return np.exp(z) / np.sum(np.exp(z), axis=1, keepdims=True)
 
-
-# Define the layers used in this model
+#Base layer class
 class Layer(object):
-    """Base class for the different layers.
-    Defines base methods and documentation of methods."""
 
+    #Return iterator over parameters
     def get_params_iter(self):
-        """Return an iterator over the parameters (if any).
-        The iterator has the same order as get_params_grad.
-        The elements returned by the iterator are editable in-place."""
         return []
 
+    #Return gradient of parameters
     def get_params_grad(self, X, output_grad):
-        """Return a list of gradients over the parameters.
-        The list has the same order as the get_params_iter iterator.
-        X is the input.
-        output_grad is the gradient at the output of this layer.
-        """
         return []
 
     def get_output(self, X):
-        """Perform the forward step linear transformation.
-        X is the input."""
         pass
 
+    #Return gradient at inputs of layer
     def get_input_grad(self, Y, output_grad=None, T=None):
-        """Return the gradient at the inputs of this layer.
-        Y is the pre-computed output of this layer (not needed in this case).
-        output_grad is the gradient at the output of this layer
-         (gradient at input of next layer).
-        Output layer uses targets T to compute the gradient based on the
-         output error instead of output_grad"""
+
         pass
 
-
+#Linear transformation
 class LinearLayer(Layer):
-    """The linear layer performs a linear transformation to its input."""
 
     def __init__(self, n_in, n_out):
-        """Initialize hidden layer parameters.
-        n_in is the number of input variables.
-        n_out is the number of output variables."""
         self.W = np.random.randn(n_in, n_out) * 0.1
         self.b = np.zeros(n_out)
 
     def get_params_iter(self):
-        """Return an iterator over the parameters."""
         return itertools.chain(np.nditer(self.W, op_flags=['readwrite']),
                                np.nditer(self.b, op_flags=['readwrite']))
 
     def get_output(self, X):
-        """Perform the forward step linear transformation."""
         return X.dot(self.W) + self.b
 
     def get_params_grad(self, X, output_grad):
-        """Return a list of gradients over the parameters."""
         JW = X.T.dot(output_grad)
         Jb = np.sum(output_grad, axis=0)
+        del output_grad, X
         return [g for g in itertools.chain(np.nditer(JW), np.nditer(Jb))]
 
     def get_input_grad(self, Y, output_grad):
-        """Return the gradient at the inputs of this layer."""
         return output_grad.dot(self.W.T)
 
-
-class LogisticLayer(Layer):
-    """The logistic layer applies the logistic function to its inputs."""
-
-    def get_output(self, X):
-        """Perform the forward step transformation."""
-        return logistic(X)
-
-    def get_input_grad(self, Y, output_grad):
-        """Return the gradient at the inputs of this layer."""
-        return np.multiply(logistic_deriv(Y), output_grad)
-
+#Apply ReLu function
 class ReluLayer(Layer):
-    """The logistic layer applies the logistic function to its inputs."""
 
     def get_output(self, X):
-        """Perform the forward step transformation."""
         return relu(X)
 
     def get_input_grad(self, Y, output_grad):
-        """Return the gradient at the inputs of this layer."""
         return np.multiply(relu_deriv(Y), output_grad)
 
-
+#Apply softMax function
 class SoftmaxOutputLayer(Layer):
-    """The softmax output layer computes the classification propabilities at the output."""
+
 
     def get_output(self, X):
-        """Perform the forward step transformation."""
         return softmax(X)
 
     def get_input_grad(self, Y, T):
-        """Return the gradient at the inputs of this layer."""
         return (Y - T) / Y.shape[0]
 
     def get_cost(self, Y, T):
-        """Return the cost at the output of this output layer."""
         return - np.multiply(T, np.log(Y)).sum() / Y.shape[0]
 
 
-# Define the forward propagation step as a method.
+# Forward propagation, returns activations for each layer
 def forward_step(input_samples, layers):
-    """
-    Compute and return the forward activation of each layer in layers.
-    Input:
-        input_samples: A matrix of input samples (each row is an input vector)
-        layers: A list of Layers
-    Output:
-        A list of activations where the activation at each index i+1 corresponds to
-        the activation of layer i in layers. activations[0] contains the input samples.
-    """
-    activations = [input_samples]  # List of layer activations
-    # Compute the forward activations for each layer starting from the first
+    activations = [input_samples]
+
     X = input_samples
+    del input_samples
     for layer in layers:
-        Y = layer.get_output(X)  # Get the output of the current layer
-        activations.append(Y)  # Store the output for future processing
-        X = activations[-1]  # Set the current input as the activations of the previous layer
-    return activations  # Return the activations of each layer
+        #Y = layer.get_output(X)  # Get the output of the current layer
+        #activations.append(Y)  # Store the output for future processing
+        #X = activations[-1]  # Set the current input as the activations of the previous layer
+         Y = layer.get_output(activations[-1])
+         activations.append(Y)
+    return activations
 
-
-# Define the backward propagation step as a method
+#Backward propagation, return parameters gradient
 def backward_step(activations, targets, layers):
-    """
-    Perform the backpropagation step over all the layers and return the parameter gradients.
-    Input:
-        activations: A list of forward step activations where the activation at
-            each index i+1 corresponds to the activation of layer i in layers.
-            activations[0] contains the input samples.
-        targets: The output targets of the output layer.
-        layers: A list of Layers corresponding that generated the outputs in activations.
-    Output:
-        A list of parameter gradients where the gradients at each index corresponds to
-        the parameters gradients of the layer at the same index in layers.
-    """
-    param_grads = collections.deque()  # List of parameter gradients for each layer
-    output_grad = None  # The error gradient at the output of the current layer
-    # Propagate the error backwards through all the layers.
-    #  Use reversed to iterate backwards over the list of layers.
+    param_grads = collections.deque()
+    output_grad = None
+
     for layer in reversed(layers):
-        Y = activations.pop()  # Get the activations of the last layer on the stack
-        # Compute the error at the output layer.
-        # The output layer error is calculated different then hidden layer error.
+        Y = activations.pop()
         if output_grad is None:
             input_grad = layer.get_input_grad(Y, targets)
-        else:  # output_grad is not None (layer is not output layer)
+        else:
             input_grad = layer.get_input_grad(Y, output_grad)
-        # Get the input of this layer (activations of the previous layer)
         X = activations[-1]
-        # Compute the layer parameter gradients used to update the parameters
         grads = layer.get_params_grad(X, output_grad)
+        del X
         param_grads.appendleft(grads)
-        # Compute gradient at output of previous layer (input of current layer):
+        del grads
         output_grad = input_grad
-    return list(param_grads)  # Return the parameter gradients
+    return list(param_grads)
 
 
-# Define a method to update the parameters
+#Update parameters according to given gradient
 def update_params(layers, param_grads, learning_rate):
-    """
-    Function to update the parameters of the given layers with the given gradients
-    by gradient descent with the given learning rate.
-    """
     for layer, layer_backprop_grads in zip(layers, param_grads):
         for param, grad in zip(layer.get_params_iter(), layer_backprop_grads):
-            # The parameter returned by the iterator point to the memory space of
-            #  the original layer and can thus be modified inplace.
             param -= learning_rate * grad  # Update each parameter
+
+#Evaluate network results
+def evaluate(test_labels, predictions):
+    precision = precision_score(test_labels, predictions, average='micro')
+    recall = recall_score(test_labels, predictions, average='micro')
+    f1 = f1_score(test_labels, predictions, average='micro')
+    print("Micro-average quality numbers")
+    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+
+    precision = precision_score(test_labels, predictions, average='macro')
+    recall = recall_score(test_labels, predictions, average='macro')
+    f1 = f1_score(test_labels, predictions, average='macro')
+    print("Macro-average quality numbers")
+    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
 
 
 def neural_network(dataset, hidden_layers):
@@ -204,36 +144,22 @@ def neural_network(dataset, hidden_layers):
         exit(1)
 
     #params:
-    batch_size = 32
-    max_nb_of_iterations = 1000  # Train for a maximum of 300 iterations
-    learning_rate = 0.5 #Gradient descent learning rate
+    batch_size = 16
+    max_nb_of_iterations = 100
+    learning_rate = 0.1
 
     data = np.array(dataset.iloc[:, 3:-1])
     target = dataset.iloc[:,-1]
-    labels = set(target)
+    del dataset
 
     #Convert target to output softmax layer format
     T = np.array(pd.get_dummies(pd.Series(target)))
-
-    print("T:")
-    print(type(T))
-
-    digits = datasets.load_digits()
-    T2 = np.zeros((digits.target.shape[0], 10))
-    T2[np.arange(len(T2)), digits.target] += 1
-
-    print("T2:")
-    print(type(T2))
-
-    print("data:")
-    print(type(data))
-
-    print("digits.data:")
-    print(type(digits.data))
+    del target
 
     # Divide the data into a train and test set.
     X_train, X_test, T_train, T_test = train_test_split(
         data, T, test_size=0.4, random_state=42)
+    del data
     # Divide the test set into a validation set and final test set.
     X_validation, X_test, T_validation, T_test = train_test_split(
         X_test, T_test, test_size=0.5, random_state=42)
@@ -241,7 +167,8 @@ def neural_network(dataset, hidden_layers):
     print("in_dim: %d" % X_train.shape[1])
     print("out_dim %d" % T_train.shape[1])
 
-    layers = []  # Define a list of layers
+    #Create layers
+    layers = []
     # Add first hidden layer
     hidden_neurons_1 = hidden_layers[0]
     hidden_neurons_last = hidden_layers[-1]
@@ -265,37 +192,40 @@ def neural_network(dataset, hidden_layers):
     layers.append(SoftmaxOutputLayer())
 
     # Create the minibatches
-
-    nb_of_batches = X_train.shape[0] / batch_size  # Number of batches
-    # Create batches (X,Y) from the training set
+    nb_of_batches = X_train.shape[0] / batch_size
     XT_batches = zip(
-        np.array_split(X_train, nb_of_batches, axis=0),  # X samples
-        np.array_split(T_train, nb_of_batches, axis=0))  # Y targets
+        np.array_split(X_train, nb_of_batches, axis=0),
+        np.array_split(T_train, nb_of_batches, axis=0))
 
     # Perform backpropagation
-    # initalize some lists to store the cost for future analysis
-    minibatch_costs = []
+    #minibatch_costs = []
     training_costs = []
     validation_costs = []
 
-
-
     # Train for the maximum number of iterations
     for iteration in range(max_nb_of_iterations):
+        print("Iteration: %d~~~~~~~~~~~~~~~~~~~~~~" % iteration)
+        batch_nr = 0
         for X, T in XT_batches:  # For each minibatch sub-iteration
+            batch_nr = batch_nr + 1
+            print("Batch nr:%d from %d" % (batch_nr, nb_of_batches))
             activations = forward_step(X, layers)  # Get the activations
-            minibatch_cost = layers[-1].get_cost(activations[-1], T)  # Get cost
-            minibatch_costs.append(minibatch_cost)
-            param_grads = backward_step(activations, T, layers)  # Get the gradients
-            update_params(layers, param_grads, learning_rate)  # Update the parameters
+            #minibatch_cost = layers[-1].get_cost(activations[-1], T)  # Get cost
+            #minibatch_costs.append(minibatch_cost)
+            #del minibatch_cost
+            param_grads = backward_step(activations, T, layers)
+            update_params(layers, param_grads, learning_rate)
         # Get full training cost for future analysis (plots)
         activations = forward_step(X_train, layers)
         train_cost = layers[-1].get_cost(activations[-1], T_train)
         training_costs.append(train_cost)
+        del train_cost
         # Get full validation cost
         activations = forward_step(X_validation, layers)
         validation_cost = layers[-1].get_cost(activations[-1], T_validation)
         validation_costs.append(validation_cost)
+        del validation_cost
+
         if len(validation_costs) > 3:
             # Stop training if the cost on the validation set doesn't decrease
             #  for 3 iterations
@@ -309,18 +239,26 @@ def neural_network(dataset, hidden_layers):
     y_true = np.argmax(T_test, axis=1)  # Get the target outputs
     activations = forward_step(X_test, layers)  # Get activation of test samples
     y_pred = np.argmax(activations[-1], axis=1)  # Get the predictions made by the network
+    '''
     test_accuracy = metrics.accuracy_score(y_true, y_pred)  # Test set accuracy
+    test_f1 = metrics.f1_score(y_true, y_pred)
+    test_recall = metrics.recall_score(y_true, y_pred)
     print('The accuracy on the test set is {:.2f}'.format(test_accuracy))
+    print('The f1 on the test set is {:.2f}'.format(test_f1))
+    print('The recall on the test set is {:.2f}'.format(test_recall))
+    '''
+    evaluate(y_true, y_pred)
+
 
     import matplotlib.pyplot as plt
     plt.figure(1)
     plt.subplot(211)
     plt.plot(validation_costs)
-    plt.label('validation_costs')
+    plt.ylabel('validation_costs')
 
     plt.subplot(212)
     plt.plot(training_costs)
-    plt.label('training_costs')
+    plt.ylabel('training_costs')
     plt.show()
 
     return
